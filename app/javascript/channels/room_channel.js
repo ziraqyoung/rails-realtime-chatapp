43724f26552ef843cf37de2f73d90b7ae5b4bf54
josphat-kariuki <me_ziraq@yahoo.com>
2019-10-20 02:53:11 +0300

    Render message's sender username in realtime in javascript

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,40 +1,42 @@
 import consumer from "./consumer";
 
 $(document).on("turbolinks:load", function() {
-    var $element = $('[data-channel-subscribe="room"]');
-    var room_id = $element.data("room-id");
+  var $element = $('[data-channel-subscribe="room"]');
+  var room_id = $element.data("room-id");
 
-    consumer.subscriptions.create(
-      {
-        channel: "RoomChannel",
-        room: room_id
+  consumer.subscriptions.create(
+    {
+      channel: "RoomChannel",
+      room: room_id
+    },
+
+    {
+      connected() {
+        console.log("connected");
+        // Called when the subscription is ready for use on the server
       },
 
-      {
-        connected() {
-          console.log("connected");
-          // Called when the subscription is ready for use on the server
-        },
+      disconnected() {
+        // Called when the subscription has been terminated by the server
+      },
 
-        disconnected() {
-          // Called when the subscription has been terminated by the server
-        },
-
-        received(data) {
-          var content = `<div class="flex items-center m-2">
+      received(data) {
+        var content = `<div class="flex items-center m-2">
             <div class="p-3 rounded-full bg-teal-200">
               <img src="${data.user_avatar_url}" class="h-8 w-8 rounded-full object-cover object-fit" alt="Profile" />
             </div>     
             <div class="ml-1 w-full p-2 rounded-lg bg-teal-200 text-gray-900">
-              <div data-role="message-text" class="text-base">${data.message}</div>
               <div class="text-sm text-right">
-                <small data-role="message-date">${data.updated_at}</small>
+                <small class="leading-tight text-gray-600 uppercase" data-role="message-date">${data.username}</small>
+              </div>
+            <div data-role="message-text" class="text-base">${data.message}</div>
+              <div class="text-sm text-right">
+                <small class="leading-tight text-gray-600" data-role="message-date">${data.updated_at}</small>
               </div>
             </div>
           </div>`;
-          $element.append(content);
-        }
+        $element.append(content);
       }
-    );
+    }
+  );
 });
-
--- app/models/room_message.rb
+++ app/models/room_message.rb
@@ -3,6 +3,8 @@ class RoomMessage < ApplicationRecord
   belongs_to :room, inverse_of: :room_messages
 
   def as_json(options)
-    super(options).merge(user_avatar_url: user.gravatar_url)
+    super(options).merge(
+      username: user.username, user_avatar_url: user.gravatar_url
+    )
   end
 end
