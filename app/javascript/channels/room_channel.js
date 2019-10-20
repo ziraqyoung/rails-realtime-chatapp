5b92503c3a12ea7ac90eabe3c522f1004a0684cd
josphat-kariuki <me_ziraq@yahoo.com>
2019-10-20 03:06:58 +0300

    Add dayjs package and from message sent date relative from current time

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,4 +1,7 @@
 import consumer from "./consumer";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
 
 $(document).on("turbolinks:load", function() {
   var $element = $('[data-channel-subscribe="room"]');
@@ -23,15 +26,23 @@ $(document).on("turbolinks:load", function() {
       received(data) {
         var content = `<div class="flex items-center m-2">
             <div class="p-3 rounded-full bg-teal-200">
-              <img src="${data.user_avatar_url}" class="h-8 w-8 rounded-full object-cover object-fit" alt="Profile" />
+              <img src="${
+                data.user_avatar_url
+              }" class="h-8 w-8 rounded-full object-cover object-fit" alt="Profile" />
             </div>     
             <div class="ml-1 w-full p-2 rounded-lg bg-teal-200 text-gray-900">
               <div class="text-sm text-right">
-                <small class="leading-tight text-gray-600 uppercase" data-role="message-date">${data.username}</small>
+                <small class="leading-tight text-gray-600 uppercase" data-role="message-date">${
+                  data.username
+                }</small>
               </div>
-            <div data-role="message-text" class="text-base">${data.message}</div>
+            <div data-role="message-text" class="text-base">${
+              data.message
+            }</div>
               <div class="text-sm text-right">
-                <small class="leading-tight text-gray-600" data-role="message-date">${data.updated_at}</small>
+                <small class="leading-tight text-gray-600" data-role="message-date">${dayjs(
+                  data.updated_at
+                ).fromNow()}</small>
               </div>
             </div>
           </div>`;
